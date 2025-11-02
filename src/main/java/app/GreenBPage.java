c456990ecf78f7b43ee914ee9cdd5b9b2334fcc7
Akith Senaratne <s4169755@student.rmit.edu.au>
2025-11-02 21:32:53 +1100

    removed comments from workshop code on akith's pages

--- src/main/java/app/GreenBPage.java
+++ src/main/java/app/GreenBPage.java
@@ -7,39 +7,27 @@ import java.util.Map;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 
-/**
- * Example Index HTML class using Javalin
- * <p>
- * Generate a static HTML page using Javalin
- * by writing the raw HTML into a Java String object
- *
- * @author Timothy Wiley, 2023. email: timothy.wiley@rmit.edu.au
- * @author Santha Sumanasekara, 2021. email: santha.sumanasekara@rmit.edu.au
- */
+
 public class GreenBPage implements Handler {
 
-    // URL of this page relative to http://localhost:7001/
+    
     public static final String URL = "/infectiondata.html";
 
-    // Name of the Thymeleaf HTML template page in the resources folder
+    
     private static final String TEMPLATE = ("GreenBPage.html");
 
     @Override
     public void handle(Context context) throws Exception {
-        // The model of data to provide to Thymeleaf.
-        // In this example the model will be filled with:
-        //  - Title to give to the h1 tag
-        //  - Array list of all movies for the UL element
+       
         Map<String, Object> model = new HashMap<String, Object>();
 
-        // Add in title for the h1 tag to the model
+        
         model.put("title", new String("About Us"));
 
-        // Look up some information from JDBC
-        // First we need to use your JDBCConnection class
+       
         JDBCConnection jdbc = new JDBCConnection();
 
-        // Next we will ask this *class* for the movies
+      
         ArrayList<Personasdata> personas = jdbc.getPersonasdata();
         ArrayList<Studentsdata> students = jdbc.getStudentsdata();
          ArrayList<String> Snames = new ArrayList<String>();
@@ -85,7 +73,7 @@ public class GreenBPage implements Handler {
       
       
 
-        // Finally put all of these movies into the model
+        
              model.put("pname1", Pnames.get(0));
         model.put("occupation1", Occupations.get(0));
         model.put("age1", Ages.get(0));
@@ -109,8 +97,9 @@ public class GreenBPage implements Handler {
 
         model.put("sname2", Snames.get(1));
         model.put("id2", ids.get(1));
-        // DO NOT MODIFY THIS
-        // Makes Javalin render the webpage using Thymeleaf
+        
+
+        
         context.render(TEMPLATE, model);
           
     }
--- src/main/java/app/Personasdata.java
+++ src/main/java/app/Personasdata.java
@@ -1,23 +1,17 @@
 package app;
 
-/**
- * Class represeting a Movie from the Movies database
- * For simplicity this uses public fields
- * You could use private fields with getters to be safer
- *
- * @author Timothy Wiley, 2023. email: timothy.wiley@rmit.edu.au
- */
+
 public class Personasdata {
-   // Movie id
+  
    public String name;
 
-   // Movie name
+  
    public String occupation;
 
-   // Year the movie was made
+ 
    public int age;
 
-   // String representing the movie genre
+   
    public String gender;
 
    public String location;
@@ -27,16 +21,11 @@ public class Personasdata {
    public String goals;
    
    public String needs;
-   /**
-    * Create a movie withou setting any of the fields,
-    * Fields will have default "empty" values
-    */
+   
    public Personasdata() {
    }
 
-   /**
-    * Create a movie setting all of the fields
-    */
+ 
    public Personasdata(String name, String occupation, int age, String gender, String location, String bio, String needs, String goals) {
       this.name = name;
       this.occupation = occupation;
